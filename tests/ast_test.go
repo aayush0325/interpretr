52ffe317b963f64306809a74884b279959d591f7
Aayush Khanna <ayush25khanna@gmail.com>
2025-07-01 12:33:47 +0530

    test: add test for AST validation

---
+++ tests/ast_test.go
@@ -0,0 +1,40 @@
+package tests
+
+import (
+	"interpretr/ast"
+	"interpretr/token"
+	"testing"
+)
+
+func TestAST(t *testing.T) {
+
+	// Good demonstration of how the AST is constructed
+	program := &ast.Program{
+		Statements: []ast.Statement{
+			&ast.LetStatement{
+				Token: token.Token{
+					Type:    token.LET,
+					Literal: "let",
+				},
+				Name: &ast.Identifier{
+					Token: token.Token{
+						Type:    token.IDENT,
+						Literal: "myVar",
+					},
+					Value: "myVar",
+				},
+				Value: &ast.Identifier{
+					Token: token.Token{
+						Type:    token.IDENT,
+						Literal: "anotherVar",
+					},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+
+	if program.String() != "let myVar = anotherVar;" {
+		t.Errorf("program.String() wrong. got=%q", program.String())
+	}
+}
